7b4aa0d345dabe847e7f56718df056314756fb1e
Vladimir <noise123q@mail.ru>
2022-06-22 19:19:54 +0400

    add aside block

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,15 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from "swiper";
 
 const swiper = new Swiper();
+
+//slider
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
